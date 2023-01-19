ce2708748541e7ffce144e03b1101b1444da397a
Filip Timofiejew <ftimofiejew@gmail.com>
2023-01-19 23:50:51 +0100

    Enable OpenApi + Enable initial Spring Security config

---
+++ src/main/java/com/movierental/spring/configuration/OpenApiConfig.java
@@ -0,0 +1,16 @@
+package com.movierental.spring.configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI();
+    }
+}
---
+++ src/main/java/com/movierental/spring/configuration/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.movierental.spring.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests().requestMatchers("/**").permitAll();
+
+        return http.build();
+    }
+}
